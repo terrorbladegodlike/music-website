418226bd2161ff56bf2beca911327150ddddd53e
terrorbladegodlike <ternovetchii.2002@mail.ru>
2023-10-15 10:56:22 +0300

    Adding the "Player" component
    When creating the "Player" component, such tools as: "Next-Image, Audio-Player, Framer-Motion".

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import Hero from '@/components/Hero'
+import Player from '@/components/Player'
 import Events from '@/components/events/Events'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Player />
       <Events />
     </main>
   )
